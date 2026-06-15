44726212af0e50f87733333a0887a94becf06b66
Stone211-cell <suvapat1905@gmail.com>
2026-06-15 23:09:46 +0700

    fix: make RootLayout async

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,7 +75,7 @@ export const viewport = {
   initialScale: 1,
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
